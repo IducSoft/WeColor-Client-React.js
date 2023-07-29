25cdde8fa2495cbcccaf01ebfdd5bafb55f34c4f
Isaac Urdaneta <urdanetacuarteisaacdavid@gmail.com>
2023-07-29 19:22:11 -0400

    trabajando en deleteAccount

--- src/Components/Views/UpdateProfile/DeleteAccount.jsx
+++ src/Components/Views/UpdateProfile/DeleteAccount.jsx
@@ -1,43 +1,71 @@
 import React from 'react'
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
+import axios from 'axios';
 
 
 
 const DeleteAccount = () => {
+  const url = "https://wecolor-api-rest.onrender.com/api";
 
   const validationSchema = Yup.object().shape({
+    email: Yup.string()
+    .email("Please enter a valid email")
+    .required('Email Address is Required'),
     password: Yup.string()
     .required('Password is required')
   });
 
+  const deleteAccount = async ({email, password})  =>{
+
+    try{
+      const response = await axios.delete(`${url}/users/delete/account`,{
+        withCredentials: true,
+        credentials: "include",
+        email, 
+        password,
+      });
+      console.log(response)
+      
+    }catch(error){
+      console.log(error)
+    }
+  }
+
     return (
-    <Formik
-      initialValues={{
-        password:"",
-      }}
-      validationSchema={validationSchema}
-      onSubmit={(values, {resetForm}) => {
-        console.log(values);
-        resetForm();
-      }}
-    >
-      {({ values, errors }) => (
-        <Form>
-          <section className='w-[300px] md:w-[500px] mx-auto my-7 border-black border-2 px-4 py-6'>
-          <h2 className='text-center my-3'>DELETE ACCOUNT</h2>
-            <label className="w-full block  text-[1rem] font-bold mb-2">
-                <p className='w-full text-center my-3'>Delete your account and all your palettes permanently.</p>
-                <Field className="my-5 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="text" name="password" values={values.password} />
-                {errors &&  <ErrorMessage name="password" component="div" />}
-            </label>
-            <button type="submit" className="w-full bg-purple-500 text-white rounded-md p-2 my-5">DELETE ACCOUNT</button>
-          </section>
-        </Form>
-      )}
-    </Formik>
-  
-)
+      <Formik
+        initialValues={{
+          email:"",
+          password:"",
+        }}
+        validationSchema={validationSchema}
+        onSubmit={ async (values, {resetForm}) => {
+          console.log(values);
+          await deleteAccount(values)
+          resetForm();
+        }}
+      >
+        {({ values, errors }) => (
+          <Form>
+            <section className='w-[300px] md:w-[500px] mx-auto my-7 border-black border-2 px-4 py-6'>
+            <h2 className='text-center my-3'>DELETE ACCOUNT</h2>
+            <p className='w-full text-center my-3'>Delete your account and all your palettes permanently.</p>
+              <label className="w-full block  text-[1rem] font-bold mb-2">
+                  <p className='w-full text-center my-3'>Enter you email</p>
+                  <Field className="my-5 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" name="email" values={values.email} />
+                  {errors &&  <ErrorMessage name="email" component="div" />}
+              </label>
+              <label className="w-full block  text-[1rem] font-bold mb-2">
+                  <p className='w-full text-center my-3'>Enter your password</p>
+                  <Field className="my-5 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" name="password" values={values.password} />
+                  {errors &&  <ErrorMessage name="password" component="div" />}
+              </label>
+              <button type="submit" className="w-full bg-purple-500 text-white rounded-md p-2 my-5">DELETE ACCOUNT</button>
+            </section>
+          </Form>
+        )}
+      </Formik>
+    )
 }
 
 export default DeleteAccount
